b3f680aae8dc83c36ee9b3a0552a85007810ef96
Ramkumar S <ramkumarcse07@gmail.com>
2024-10-04 02:11:24 +0530

    Replaced double quotes with single quotes

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -3,18 +3,18 @@ import Image from "next/image";
 
 const MovieCard = ({ movie }) => {
   return (
-    <div className="bg-gray-800 rounded-lg overflow-hidden shadow-lg">
+    <div className='bg-gray-800 rounded-lg overflow-hidden shadow-lg'>
       <Link href={`/movie/${movie.id}`}>
         <Image
-          className="w-full h-72 object-cover"
+          className='w-full h-72 object-cover'
           src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
           alt={movie.title}
         />
-        <div className="p-4">
-          <h2 className="text-lg font-semibold text-white truncate">
+        <div className='p-4'>
+          <h2 className='text-lg font-semibold text-white truncate'>
             {movie.title}
           </h2>
-          <p className="text-gray-400 mt-2">{movie.release_date}</p>
+          <p className='text-gray-400 mt-2'>{movie.release_date}</p>
         </div>
       </Link>
     </div>
